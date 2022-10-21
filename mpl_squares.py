17d9bb9e0a6c56dcefa5eff16f15b6348a5814f1
o_w <womarow@gmail.com>
2022-10-21 16:28:04 +0300

    styles

--- mpl_squares.py
+++ mpl_squares.py
@@ -7,11 +7,14 @@ SEQUENCE 1, 4, 9, 16, 25 AS THE DATA FOR THE GRAPH.
 
 import matplotlib.pyplot as plt
 
-squares = [1,4,9,16,25,36,49,64,81,100,121,144.169,196,225,256,289,324,361,400,441,484,529,576,625,676,729,784,841,900,961]
+input_values = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
+
+squares = [1,4,9,16,25,36,49,64,81,100,121,144,169,196,225,256,289,324,361,400,441,484,529,576,625,676,729,784,841,900,961]
+plt.style.use('seaborn')
 
 fig, ax = plt.subplots()
 
-ax.plot(squares,linewidth=3)
+ax.plot(input_values,squares,linewidth=3)
 ax.set_title("SQUARE OF VALUE",fontsize=15)
 ax.set_xlabel("VALUE",fontsize=15)
 ax.set_ylabel("SQUARE OF VALUE",fontsize=14)
